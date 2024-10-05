7d7ee3e4559bb6e4c47dbecf90f33c74c7fbff31
Sam Blundell <samcalblundell@googlemail.com>
2024-10-05 21:11:29 +0100

    basic movement

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (255, 0, 255))
+
+        player.update(dt)
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
